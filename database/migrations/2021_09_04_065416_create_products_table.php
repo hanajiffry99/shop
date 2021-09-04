0701547ee3ed578aca90e82a9ae8910188fae128
Hana Jiffry <hanajiffryscg@gmail.com>
2021-09-04 12:28:40 +0530

    initial setup,models & migrations

---
+++ database/migrations/2021_09_04_065416_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->integer('id')->unsigned()->unique();
+            $table->string('product_name');
+            $table->integer('price')->unsigned();
+            $table->integer('available')->unsigned()->comment("0-no,1-yes");
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
