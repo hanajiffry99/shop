5b95f108bb5a71404d86ec667b74ddac9c2ed782
Hana Jiffry <hanajiffryscg@gmail.com>
2021-09-04 12:34:33 +0530

    set routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products/{id?}',[ProductController::class,'getproducts']);
+Route::post('/addproducts',[ProductController::class,'addproducts']);
+Route::put('/editproducts',[ProductController::class,'editproducts']);
+Route::delete('/products/{id}',[ProductController::class,'deleteproduct']);
+Route::get('/search/{name}',[ProductController ::class,'searchproduct']);
+
